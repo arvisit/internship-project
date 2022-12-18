2e6477db91b2441b2660f9d52c0bf6507fd37bbd
Alexander Nenartovich <s-afer@yandex.ru>
2022-12-19 00:21:24 +0300

    replacing LocalDateTime with ZonedDateTime

--- src/main/java/by/itacademy/profiler/api/controllers/GlobalExceptionHandler.java
+++ src/main/java/by/itacademy/profiler/api/controllers/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +22,7 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
                 exception.getMessage(),
-                LocalDateTime.now());
+                ZonedDateTime.now());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
