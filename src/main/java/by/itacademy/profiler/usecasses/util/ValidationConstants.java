949ea103ac8d6eebfacbb8c78f17ccec0309638a
Alexander Nenartovich <s-afer@yandex.ru>
2022-12-29 21:16:05 +0000

    [PROF2022-378] Email validation re-fixed, regex moved to separate "Constants" class

---
+++ src/main/java/by/itacademy/profiler/usecasses/util/ValidationConstants.java
@@ -0,0 +1,5 @@
+package by.itacademy.profiler.usecasses.util;
+
+public class ValidationConstants {
+    public static final String REGEXP_VALIDATE_EMAIL = "^[\\w ]+([!\"#$%&'()*+,\\-./:;<=>?\\[\\]\\\\^_{}][a-z0-9]+)*@[a-zA-Z0-9-_]+\\.+([a-zA-Z]{2,6}+[ +]*)$";
+}
