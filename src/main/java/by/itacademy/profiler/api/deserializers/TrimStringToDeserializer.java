982c710483e24ecc06487b68df939fe6def25642
Alexander Nenartovich <s-afer@yandex.ru>
2023-01-08 17:16:29 +0000

    [PROF2022-384] Added handling of null

--- src/main/java/by/itacademy/profiler/api/deserializers/TrimStringToDeserializer.java
+++ src/main/java/by/itacademy/profiler/api/deserializers/TrimStringToDeserializer.java
@@ -2,23 +2,37 @@ package by.itacademy.profiler.api.deserializers;
 
 import by.itacademy.profiler.usecasses.dto.AuthenticationRequestDto;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 
+import static java.util.Objects.isNull;
+
 @JsonComponent
 public class TrimStringToDeserializer extends JsonDeserializer<AuthenticationRequestDto> {
+    private JsonNode jsonNode;
 
     @Override
     public AuthenticationRequestDto deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
             throws IOException {
-        TreeNode treeNode = jsonParser.getCodec().readTree(jsonParser);
-        TextNode email = (TextNode) treeNode.get("email");
-        TextNode password = (TextNode) treeNode.get("password");
-        return new AuthenticationRequestDto(email.asText().trim(), password.asText());
+        jsonNode = jsonParser.getCodec().readTree(jsonParser);
+        String email = (getValue("email"));
+        String password = getValue("password");
+        if (!isNull(email)) {
+            email = email.trim();
+        }
+        return new AuthenticationRequestDto(email, password);
+    }
+
+    private String getValue(String s) {
+        if (!isNull(jsonNode.get(s))) {
+            if (!jsonNode.get(s).isNull()) {
+                return jsonNode.get(s).asText();
+            }
+        }
+        return null;
     }
 }
