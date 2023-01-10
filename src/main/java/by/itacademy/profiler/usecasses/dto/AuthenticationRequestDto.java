baa9f4b8e9101ca0ffdbb98e3235d4b2a4d91b33
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2023-01-10 14:41:46 +0300

    PROF2022-707 Improved email regexp

--- src/main/java/by/itacademy/profiler/usecasses/dto/AuthenticationRequestDto.java
+++ src/main/java/by/itacademy/profiler/usecasses/dto/AuthenticationRequestDto.java
@@ -2,7 +2,6 @@ package by.itacademy.profiler.usecasses.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
@@ -11,7 +10,6 @@ import static by.itacademy.profiler.usecasses.util.ValidationConstants.REGEXP_VA
 public record AuthenticationRequestDto(
         @NotBlank(message = "Email is mandatory!")
         @Pattern(regexp = REGEXP_VALIDATE_EMAIL, message = "Invalid e-mail address")
-        @Size(max = 50, message = "Invalid e-mail address")
         String email,
 
         @NotBlank(message = "Password is mandatory!")
--- src/main/java/by/itacademy/profiler/usecasses/util/ValidationConstants.java
+++ src/main/java/by/itacademy/profiler/usecasses/util/ValidationConstants.java
@@ -1,7 +1,7 @@
 package by.itacademy.profiler.usecasses.util;
 
 public class ValidationConstants {
-    public static final String REGEXP_VALIDATE_EMAIL = "^[\\s]*[a-zA-Z0-9]+([!\"#$%&'()*+,\\-./:;<=>?\\[\\]\\^_{}][a-zA-z0-9]+)*@([\\w]+(-[\\w]+)?)(\\.[a-z]{2,})+[\\s]*$";
+    public static final String REGEXP_VALIDATE_EMAIL = "^(?=.{6,50}$)[\\s]*[a-zA-Z0-9]+([!\"#$%&'()*+,\\-.\\/:;<=>?\\[\\]\\^_{}][a-zA-z0-9]+)*@([\\w]+(-[\\w]+)?)(\\.[\\w]+[-][\\w]+)*(\\.[a-z]{2,})+[\\s]*$";
 
     public static final String REGEXP_VALIDATE_NAME = "^(?=.{1,50}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$";
 
