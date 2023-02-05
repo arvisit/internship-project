425931625defe9e11502b0d7402e91af65e4f8ae
Alexey Iyunski <justshooter@gmail.com>
2023-02-05 17:34:12 +0000

    PROF2022-531 Bug fix

--- src/main/java/by/itacademy/profiler/usecasses/util/ValidationConstants.java
+++ src/main/java/by/itacademy/profiler/usecasses/util/ValidationConstants.java
@@ -7,13 +7,13 @@ import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 public class ValidationConstants {
-    public static final String REGEXP_VALIDATE_EMAIL = "^(?=.{6,50}$)[\\s]*[a-zA-Z0-9]+([!\"#$%&'()*+,\\-.\\/:;<=>?\\[\\]\\^_{}][a-zA-z0-9]+)*@([\\w]+(-[\\w]+)?)(\\.[\\w]+[-][\\w]+)*(\\.[a-z]{2,})+[\\s]*$";
+    public static final String REGEXP_VALIDATE_EMAIL = "^(?=.{6,}$)[\\s]*[a-zA-Z0-9]+([!\"#$%&'()*+,\\-.\\/:;<=>?\\[\\]\\^_{}][a-zA-z0-9]+)*@([\\w]+(-[\\w]+)?)(\\.[\\w]+[-][\\w]+)*(\\.[a-z]{2,})+[\\s]*$";
 
-    public static final String REGEXP_VALIDATE_NAME = "^(?=.{1,50}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$";
+    public static final String REGEXP_VALIDATE_NAME = "^(?=.{1,}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$";
 
-    public static final String REGEXP_VALIDATE_SURNAME = "^(?=.{1,50}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$";
+    public static final String REGEXP_VALIDATE_SURNAME = "^(?=.{1,}$)[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$";
 
-    public static final String REGEXP_VALIDATE_CELL_PHONE = "^(?=.{1,25}$)[0-9]*$";
+    public static final String REGEXP_VALIDATE_CELL_PHONE = "^(?=.{1,}$)[0-9]*$";
 
     public static final List<String> CONTENT_TYPES = Arrays.asList(IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE);
 
