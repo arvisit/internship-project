c9b7c4973245a6dc7d0f508120b4d13e310f19e0
Alexander Nenartovich <s-afer@yandex.ru>
2023-02-06 10:46:50 +0000

    [prof2022 748] Corrected the order of phone codes

--- src/main/java/by/itacademy/profiler/persistence/repository/PhoneCodeRepository.java
+++ src/main/java/by/itacademy/profiler/persistence/repository/PhoneCodeRepository.java
@@ -2,6 +2,12 @@ package by.itacademy.profiler.persistence.repository;
 
 import by.itacademy.profiler.persistence.model.PhoneCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PhoneCodeRepository extends JpaRepository<PhoneCode, Long> {
+    @Query("SELECT p FROM PhoneCode p JOIN Country c ON p.country.id = c.id " +
+            "ORDER BY FIELD(c.countryName,'Lithuania','Latvia','Ukraine','Russia','Belarus') DESC")
+    List<PhoneCode> findAll();
 }
