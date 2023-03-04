39f150e99dc3f914cb5391c2739bc57780d825cd
Alexey Iyunski <justshooter@gmail.com>
2023-03-04 09:52:19 +0000

    PROF2022-1380 Bug-Fix

--- src/main/java/by/itacademy/profiler/usecasses/dto/CurriculumVitaeRequestDto.java
+++ src/main/java/by/itacademy/profiler/usecasses/dto/CurriculumVitaeRequestDto.java
@@ -5,6 +5,7 @@ import by.itacademy.profiler.usecasses.annotation.PositionValidation;
 import by.itacademy.profiler.usecasses.annotation.UserImageValidation;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
 
@@ -14,10 +15,12 @@ import static by.itacademy.profiler.usecasses.util.ValidationConstants.REGEXP_VA
 
 public record CurriculumVitaeRequestDto(
         @UserImageValidation(toValidate = UserImageValidation.ValidatedDto.CV_DTO) String imageUuid,
-        @NotNull
+        @Length(max = 50, message = "Maximum length of name is 50 symbols")
+        @NotNull(message = "Field must to be filled")
         @Pattern(regexp = REGEXP_VALIDATE_NAME, message = "Invalid name")
         String name,
-        @NotNull
+        @Length(max = 50, message = "Maximum length of surname is 50 symbols")
+        @NotNull(message = "Field must to be filled")
         @Pattern(regexp = REGEXP_VALIDATE_SURNAME, message = "Invalid surname")
         String surname,
         @NotNull
