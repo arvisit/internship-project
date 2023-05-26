2f53d863408f0a1c57edc5f1941b04886caa6894
Artem Sitko <sitko.ar@gmail.com>
2023-05-26 14:36:18 +0300

    Remove excessive container creating

--- src/test/java/by/itacademy/profiler/MysqlSQLTestContainerExtension.java
+++ src/test/java/by/itacademy/profiler/MysqlSQLTestContainerExtension.java
@@ -3,17 +3,11 @@ package by.itacademy.profiler;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.containers.MySQLContainer;
 
 public class MysqlSQLTestContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) {
-        MySQLContainer<?> container =
-                new MySQLContainer<>("mysql:8.0")
-                        .withDatabaseName("profiler-db");
-
-        container.start();
 
         System.setProperty("spring.datasource.url", "jdbc:tc:mysql:8:///");
         System.setProperty("spring.liquibase.change-log", "classpath:/db.changelog/changelog-test.xml");
